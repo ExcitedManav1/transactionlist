3e9a1f36fde2a546940008c45022563f0b8f8f72
Vidhan Gupta <kundanguptaelec@gmail.com>
2022-01-18 14:29:33 +0530

    Text1

---
+++ android/app/src/main/kotlin/com/mbo/transactionlist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mbo.transactionlist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+void main() {
+  runApp(MyHonePage());
+}
+
+class MyHonePage extends StatelessWidget {
+  const MyHonePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Your Transactions',
+            style: GoogleFonts.dongle(fontSize: 40),
+          ),
+        ),
+        body: Column(
+          children: <Widget>[
+            Card(
+              child: Container(
+                child: Text('Chart'),
+                width: double.infinity,
+              ),
+              color: Color(0xFFF2F2F2),
+            ),
+            Card(
+              child: Text('List of TX'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
